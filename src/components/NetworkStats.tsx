6066f3677bb308f1539704ab4f5d349c69b22272
NuZard84 <patelhetvarasada@gmail.com>
2025-06-10 20:07:12 +0530

    Fix: color change of plan title

--- src/components/NetworkStats.tsx
+++ src/components/NetworkStats.tsx
@@ -27,13 +27,15 @@ const StatCard = ({
 }: StatCardProps) => {
   let isPlan = title === "Your Plan";
 
+  console.log("value", value);
+
   const getColor = () => {
     if (isPlan) {
       if (value === "Basic") {
         return "text-white";
       } else if (value === "Ultimate") {
         return "text-yellow-400";
-      } else if (value === "Enterprise") {
+      } else if (value === "Enterprice") {
         return "text-green-400";
       } else {
         return "text-white";
